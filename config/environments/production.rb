1751e82c345cf1231c4aa2681546fea22a3b95ac
joeman29 <yosef29@gmail.com>
2014-01-14 11:53:03 +0200

    more of the same

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ WOtracker::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  #for heroku:
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # Generate digests for assets URLs.
   config.assets.digest = true
