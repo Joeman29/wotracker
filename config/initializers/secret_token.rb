9da053ce5e33222a73834ac0d97f8f3681946bc0
joeman29 <yosef29@gmail.com>
2014-01-14 10:25:56 +0200

    Add in heroku required files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WOtracker::Application.config.secret_key_base = '8eb63ef4c4f318589acc72440937741b74b2a01b142bafba79c1cd474ed1d5b4337cd8cb1fcfa1007f835c100faf0ba6dcfbf00e60b32a7652cb721b4c78d641'
